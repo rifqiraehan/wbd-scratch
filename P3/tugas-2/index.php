207b58a38819ce4059b13e120c5ed5ff92b1d483
rifqiraehan <rifqiraehan86@gmail.com>
2024-04-23 12:08:51 +0700

    update tampilan tugas-2

--- P3/tugas-2/index.php
+++ P3/tugas-2/index.php
@@ -6,9 +6,16 @@
     <title>Demo Operator</title>
 </head>
 <body>
-    <h1>Kalkulator Sederhana</h1>
+    <h1>Demo Operator</h1>
+    <h1>Artihmetic</h1>
     <form action="proses.php" method="POST">
-        <table>
+        <table border="1">
+            <thead>
+                <th>Operand Kiri</th>
+                <th>Operator</th>
+                <th>Operand Kanan</th>
+                <th></th>
+            </thead>
             <tr>
                 <td><input type="number" name="operand_kiri" required></td>
                 <td>
